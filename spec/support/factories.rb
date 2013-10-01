689657ab082f0906889d63962e58a9bce616692b
akainic <annekainic@gmail.com>
2013-10-01 12:56:04 -0400

    Installed simple form

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     username 'kalequeen'
     email 'kalequeen@example.com'
-    password 'Greens100'
-    password_confirmation 'Greens100'
+    password 'Greens101'
+    password_confirmation 'Greens101'
   end
 end
