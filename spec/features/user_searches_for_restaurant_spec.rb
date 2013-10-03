dc65cc41f07fcc7cd5d2a4f3a1e9ed6f45735578
akainic <annekainic@gmail.com>
2013-10-03 16:10:10 -0400

    Wrote acceptance tests for searching by name

---
+++ spec/features/user_searches_for_restaurant_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'user searches for restaurant', %Q{
+  As a registered or unregistered user
+  I want to be able to search for a restaurant by name
+  So that I can see the restaurant's profile and ratings
+} do 
+
+  # Acceptance Criteria:
+  #
+  # * I must enter a restaurant name in the search bar 
+  #   and hit the search button
+  # * If I enter a restaurant name, I will be redirected 
+  #   to a page with the matching result or results
+  # * If I enter a restaurant name and there are no matches, 
+  #   I will receive an error message indicating that no 
+  #   results were found
+  # * If I do not enter a restaurant name, I will receive 
+  #   an error message that I must enter some search criteria
+
+  
+
+
+
+end
