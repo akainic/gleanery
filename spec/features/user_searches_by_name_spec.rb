938d36163f78a8c81f6cb8ac64404bd7509c35a7
akainic <annekainic@gmail.com>
2013-10-08 18:04:54 -0400

    Added search form to home page, amended search results

--- spec/features/user_searches_by_name_spec.rb
+++ spec/features/user_searches_by_name_spec.rb
@@ -4,18 +4,18 @@ feature 'user searches for restaurant by name', %Q{
   As a registered or unregistered user
   I want to be able to search for a restaurant by name
   So that I can see the restaurant's profile and ratings
-} do 
+} do
 
   # Acceptance Criteria:
   #
-  # * I must enter a restaurant name in the search bar 
+  # * I must enter a restaurant name in the search bar
   #   and hit the search button
-  # * If I enter a restaurant name, I will be redirected 
+  # * If I enter a restaurant name, I will be redirected
   #   to a page with the matching result or results
-  # * If I enter a restaurant name and there are no matches, 
-  #   I will receive an error message indicating that no 
+  # * If I enter a restaurant name and there are no matches,
+  #   I will receive an error message indicating that no
   #   results were found
-  # * If I do not enter a restaurant name, I will receive 
+  # * If I do not enter a restaurant name, I will receive
   #   an error message that I must enter some search criteria
 
   pending 'user enters valid restaurant name' do
