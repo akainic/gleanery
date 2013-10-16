f45db5b1b1f91ee5b934cc1fb763b0ff37f72881
akainic <annekainic@gmail.com>
2013-10-16 16:30:41 -0400

    Added AJAX and JQuery to calculate ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,23 +2,34 @@ class RatingsController < ApplicationController
   before_action :find_restaurant
 
   def create
-    @rating = @restaurant.ratings.build(rating_params)
-    @rating.user = current_user
-    if @rating.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      redirect_to restaurant_path(@restaurant),
-        notice: "Your vote was not saved"
+    @rating = @restaurant.ratings.find_or_initialize_by(user: current_user)
+    #@rating = @restaurant.ratings.build(rating_params)
+    #@rating.user = current_user
+
+    respond_to do |format|
+      if @rating.update(rating_params)
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.js { render :update }
+      else
+        format.html { redirect_to restaurant_path(@restaurant),
+          notice: "Your vote was not saved" }
+        format.js {}
+      end
     end
   end
 
   def update
     @rating = @restaurant.ratings.where(user: current_user).find(params[:id])
-    if @rating.update(rating_params)
-      redirect_to restaurant_path(@restaurant)
-    else
-      redirect_to restaurant_path(@restaurant),
-        notice: "Your vote was not saved"
+
+    respond_to do |format|
+      if @rating.update(rating_params)
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.js { render :update }
+      else
+        format.html { redirect_to restaurant_path(@restaurant),
+          notice: "Your vote was not saved" }
+        format.js {}
+      end
     end
   end
 
@@ -29,6 +40,6 @@ class RatingsController < ApplicationController
   end
 
   def rating_params
-    params.require(:rating).permit(:vegan, :vegetarian, :gluten_free)
+    params.require(:rating).permit(:vegan, :vegetarian, :gluten_free, :overall)
   end
 end
